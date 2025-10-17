6d75e14315a0db51b87739b9440dc5cadcb20e26
Ignacio Arena <ignacioarenasaez@gmail.com>
2025-10-17 14:41:05 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1151047492-b8ee5",
+  "appId": "1:481710665417:web:4e5d630538a01ba45b2718",
+  "apiKey": "AIzaSyDAXPr6DtP_m72fDiHeeWZUME2pzxdNeag",
+  "authDomain": "studio-1151047492-b8ee5.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "481710665417"
+};
