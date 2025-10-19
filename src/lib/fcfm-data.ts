2f7ef7a79d580350fb038ffe2dc8bb7398326ac5
ALEXIA VALENTINA ROA <alexia.roa@ug.uchile.cl>
2025-10-19 22:34:22 +0000

    Buenísmo!!! Está quedando increíble, ahora necesito que vayas incorporan

--- src/lib/fcfm-data.ts
+++ src/lib/fcfm-data.ts
@@ -49,4 +49,195 @@ Estudiantes de Postítulos y Postgrados. Estudiantes sin acreditación socioecon
 Estudiantes de ciclo básico de Artes. Estudiantes con postergaciones para el año o semestre académico vigente (a excepción de aquellos que soliciten alguna franquicia relacionada directamente con la causal que dio origen a la suspensión). Alumnos Universidad de Valparaíso, quienes sólo tienen atención gratuita en SEMDA. Alumnos CAPREDENA O DIPRECA. Alumnos en calidad de egresados y sin matrícula vigente para el año en curso.
 
 Prestaciones que no son cubiertas por franquicia: Tratamiento maxilofacial. Atención dental en el hospital clínico. Parto, intervenciones asociadas al área gineco-obstetra, o complicaciones derivadas tras procedimientos asociados a esta área. Atenciones o prestaciones con fines estéticos que no tengan relación con problemas médicos y/o de salud. Papanicolaou (PAP).
+
+Información sobre el cuerpo funcionario de la FCFM:
+1. Decanatura:
+La Decanatura es la unidad que encabeza la Facultad de Ciencias Físicas y Matemáticas. Le corresponde la dirección de esta institución, en el contexto de los lineamientos y estrategias emanados de los órganos superiores de la Universidad de Chile, sin perjuicio de las atribuciones que el Estatuto Universitario le confiere al Consejo de Facultad. 
+Integrantes:
+Decano: Francisco Martínez Concha
+Jefe de gabinete: Julio Salas Manzur
+Asistente: Doris Urrutia Rozas
+Encargada logística: Carmen Valdivia Duarte
+Chofer auxiliar: Felipe Troncoso Boza
+Contacto:
++562 2978 4425
+Av. Beauchef 850. Edificio Justicia Espada, 8vo piso
+decanato@ing.uchile.cl
+
+2. Vicedecanatura:
+La Vicedecanatura es encabezada por el vicedecano/a quien es Ministro/a de Fe de la Facultad y subrogante legal del decano/a. Dependen de esta unidad los organismos de apoyo y asesoría integral establecidos en el artículo 4º del Reglamento General de Facultades y le corresponde desempeñar las demás funciones que el decano/a expresamente le delegue. 
+Integrantes:
+Vicedecana: Marcela Munizaga Muñoz
+Asistente: Doris Urrutia Rozas
+Asistente de Gestión y Proyectos: Muriel Rivera Contreras
+Contacto:
++562 2978 4419
+Av. Beauchef 850, Santiago. Edificio Justicia Espada, 8vo piso.
+sevideca@ing.uchile.cl 
+
+3. Oficina de Arquitectura:
+La Oficina de Arquitectura, dependiente del Vicedecanato, diseña, gestiona, licita, contrata e inspecciona todos los proyectos de construcción y mantención relacionados a la Facultad de Ciencias Físicas y Matemáticas.
+Integrantes:
+Jefe de la unidad: Diego Peñailillo
+Coordinadora de proyectos estratégicos: Javiera Miranda
+Asistente Ejecutiva Administrativa: Nathalie Viedma G.
+Contacto: 
++562 29784450, +562 29780720, +562 29784637 
+2do piso, Edificio Escuela, Beauchef 850, Santiago. 
+dpenailillo@ing.uchile.cl, javiera.miranda.a@uchile.cl, nathalieviedma@uchile.cl 
+
+4. Oficina de Ingeniería para la Sustentabilidad:
+Creada el año 2013, la Oficina de Ingeniería para la Sustentabilidad (OIS) —dependiente del Vicedecanato— es un espacio de planificación, gestión y discusión de temáticas ligadas a la sustentabilidad en el marco de la Educación Superior. En ese contexto, nos caracterizamos por trabajar en conjunto con un equipo multidisciplinario compuesto por autoridades, estudiantes, académicos/as, funcionarios/as y otras organizaciones ligadas.
+Integrantes:
+Jefe de la Oficina: Pablo Garrido S.
+Asistente Administrativa: Nathalie Viedma G.
+Diseñadora: Macarena Cerda A.
+Auxiliar de Reciclaje: Jessica Cabello H.
+Auxiliar de Reciclaje: Patricio Vivanco S.
+Contacto:	
++562 29784637, +562 29780864
+Edificio Escuela, 2do piso, Beauchef 850, Santiago.
+sustentabilidad@ing.uchile.cl 
+
+5. Dirección de Vinculación Externa:
+La Dirección de Vinculación Externa tiene como rol fundamental cautelar y potenciar la articulación de la FCFM con el medio nacional e internacional, logrando una interacción efectiva y pertinente con los diversos actores de la sociedad en la que estamos inmersos. Entendiendo que las problemáticas contemporáneas y la generación de conocimiento responden a una vocación cada vez más universal, su función no distingue en su alcance lo local de lo global
+Integrantes:
+Director de Vinculación Externa: Luis Vargas Díaz
+Asistente de dirección: Daniela García de Andraca
+Contacto:
++562 29780965
+Av. Beauchef 850, Edificio Justicia Espada, piso 3, Santiago
+dirvex@fcfm.uchile.cl 
+
+6. Subdirección de Comunicaciones:
+La Subdirección de Comunicaciones es parte de la Dirección de Vinculación Externa. Es la unidad encargada de difundir el quehacer de la Facultad de Ciencias Físicas y Matemáticas a la comunidad interna y la sociedad. Su labor también contempla la divulgación científica, la extensión y la captación de nuevos y nuevas estudiantes a través de la vinculación con establecimientos educacionales.
+Integrantes:
+Subdirectora de comunicaciones: Ana María Sáez C.
+Periodista: Andrea Dávalos O.
+Periodista: Zafiro Fleming C.
+Periodista: Cristina Espinoza C.
+Diseñadora: Macarena Rivas C.
+Audiovisualista: Felipe Bawlitza V.
+Encargada de Difusión a Colegios: Pilar Valderrama L.
+Secretaria: María José Mena T.
+Secretaria Recepcionista: Silvia Pino R.
+Contacto:
++562 29780000, +562 29784615, +562 29784614, +562 29780538
+Beauchef 850, Santiago. Edificio Justicia Espada, 3er piso
+comunicaciones@fcfm.uchile.cl
+
+7. Subdirección de Gestión y Administración:
+La Subdirección de Gestión y Administración, parte de la Dirección de Vinculación Externa, es la unidad encargada de gestionar y administrar los recursos y el equipo disponible, con el objetivo de garantizar la continuidad operacional y la ejecución de los procesos administrativos de la Dirección, además de implementar y monitorear procesos, procedimientos, sistemas de control de gestión y presupuesto, tanto a nivel operacional, táctico y estratégico. Su labor también contempla la formulación, ejecución y gestión de proyectos de extensión de la Dirección, la reportabilidad del quehacer en extensión y vinculación de la Facultad, la gestión y administración de la Tienda Beauchef, y la coordinación de las acciones realizadas por el Coro FCFM y la Orquesta Beauchef.
+Integrantes:
+Subdirector de Gestión y Administración: Sergio Castillo Jara
+Coordinador de Gestión y Proyectos: Jaime Morales Paredes
+Encargada Financiera Contable: Camila Padilla Torres
+Analista Administrativo: Daniel Rivera Oróstica
+Directora Coro FCFM: Verónica Rivas Iglesias
+Director Orquesta Beauchef: Fernando Aravena Burgos
+Contacto:
++562 29780965, +562 29780526, +562 29780966
+Av. Beauchef 850, Santiago. Edificio Justicia Espada, 3er piso
+dirvex@fcfm.uchile.cl 
+
+8. Subdirección de Relaciones Institucionales:
+La Subdirección de Relaciones Institucionales de la Facultad de Ciencias Físicas y Matemáticas de la Universidad de Chile es la unidad que se encarga de establecer contactos entre la FCFM y el medio externo, tanto nacional como internacional. Forman parte del quehacer de la Subdirección, la relación con ex estudiantes (Alumni), la vinculación con el medio y las relaciones internacionales.
+Integrantes:
+Subdirectora de Relaciones Institucionales: Viviana Ruiz P.
+Coordinadora de Relaciones Internacionales: Karen Hansen F.
+Asistente Oficina de Relaciones Internacionales: Marta Cabello M.
+Coordinadora de Alumni: Lorena Barros C.
+Coordinador de Vinculación con el Medio: Carlos Muñoz N.
+Contacto:
++562 29780858, +562 29780977+, 562 29780964
+Av. Beauchef 850, Santiago. Edificio Justicia Espada, 3er piso
+dirvex@fcfm.uchile.cl 
+
+9. Oficina de Mediación Universitaria:
+La Oficina de Mediación proporciona un espacio de diálogo confidencial para ayudar a resolver los aspectos humanos de los conflictos que afectan los estudios o el trabajo. El Mediador Universitario puede brindar orientación y consejo para enfrentar un determinado conflicto interpersonal y si se le solicita, puede ejercer facultades de mediación entre miembros de la comunidad del FCFM ya sea estudiantes, funcionarios y académicos, investigadores, postdoctorados y todo aquel que tenga algún vínculo contractual con la Facultad. Tiene, además, la atribución de proponer y sugerir normativas o acciones para mejorar la convivencia al interior de nuestra Facultad. Los tipos de conflicto que aborda son, entre otros, trato inadecuado, dificultades en las relaciones laborales, comentarios sexistas, difamación, conflictos docentes, discriminaciones arbitrarias, prácticas reñidas con la ética, conflictos propios de la actividad de los académicos, faltas a las normas y reglamentos, orientación en la interpretación de reglamentos, etc.
+Integrantes:
+Mediador Universitario: Prof. José Rutllant
+Psicóloga: Carolina Díaz Herrera 
+Contacto:
++56 229777448, +5622 9777447	
+Av. Blanco Encalada 2085. Edificio interior, 3er piso.	
+maparada@ing.uchile.cl, ombuds_fcfm@ing.uchile.cl 
+
+10. Unidad de Prevención de Riesgos:
+La Unidad de Prevención de Riesgo, perteneciente a la Vicedecanatura, es responsable de la implementación, administración y mantención del Sistema de Gestión de Seguridad y Salud en el Trabajo (SST) de la FCFM; de proporcionar a la comunidad funcionaria y académica de los diferentes departamentos y unidades de la Facultad asesoría y respaldo en seguridad y salud en el trabajo especializado y dirigido a los asuntos propios de los departamentos, centros y unidades, incluyendo la proposición de objetivos de seguridad y salud en el trabajo específicos para cada uno de ellos, además de la confección y ejecución del Programa de Gestión de Riesgos de la FCFM, considerando los riesgos e impactos específicos con la finalidad de prevenir accidentes y enfermedades profesionales, de acuerdo a la normativa legal vigente.
+Integrantes:
+Jefe de unidad: Alvaro Contreras S.
+Ingeniera de unidad: Nataly Mardones O.
+Paramédico: Cesar Oyazun R.
+Contacto:	
++562 29784353, +562 29780898, +562 29777409
+Av. Beauchef 850, Santiago. Edificio Justicia Espada, 4to piso
+prevencionbeauchef@fcfm.uchile.cl 
+
+11. Unidad de Cobranzas y Aranceles:
+Esta unidad dependiente de la Tesorería de la Facultad es la encargada de efectuar la cobranza extrajudicial de los documentos no pagados a la Universidad en los plazos estipulados, y además actúa como caja auxiliar para la recepción de matrículas, aranceles y otros derechos que pagan regularmente los alumnos de la Facultad.
+Integrantes:
+Cajero: Luis Ríos G.
+Cajera: Daniza Alfaro C.
+Contacto:
++562 29784665, +562 29784666
+Beauchef 850, Santiago. Edificio Escuela, Hall Sur
+
+12. Centro de Computación, CEC:
+El CEC provee, administra y entrega soporte a la infraestructura tecnológica de la FCFM, ademas de fomentar la incorporación de tecnologías de la información que favorezcan el desarrollo de actividades docentes, administrativas, de investigación y extensión. Entre sus responsabilidades se destacan: Administración del DataCenter de la Facultad, Administración de salas de computación y sus servicios asociados, Administración de la red de datos del campus y servicios Internet principales, Instalación y mantención de la red física de la FCFM y proyectos asociados.
+Integrantes:
+Director: Sergio Miranda M.
+Jefe Área de Sistemas: Jorge Concha C.
+Área de Sistemas: Felipe Muñoz C.
+Área de Sistemas: Daniel Contreras
+Jefe Área de Redes: Hector Véliz O.
+Ingeniero de Redes: John Oliva C.
+Jefe Área Proyectos: Luis Farías C.
+Área de Proyectos: Williams González
+Técnico Cableado: David Alarcón
+Técnico Cableado: Gonzalo Ochoa
+Técnico Cableado: Julio Farías
+Técnica Cableado: Antonia Gamboa
+Jefe Área Operaciones: Hernan Mendoza O.
+Operador DataCenter: Israel Copaiba
+Operadora DataCenter: Elizabeth Lafertte G.
+Operador DataCenter: Arturo Moya G.
+Operador DataCenter: Fabian Orellana
+Operador DataCenter: Gustavo Rodriguez M.
+Operador DataCenter: Bryan Araujo
+Jefe Área Recepción: José Orellana O.
+Área de Recepción: Roque Grez S.
+Área de Recepción: Felipe Suazo V.
+Oficina Administrativa: Guillermo Zamorano A.
+Auxiliar Aseo: Erika Jiménez E.
+Contacto:
++562 29784324
+Beauchef 851, Torre Norte, piso -1
+contacto@cec.uchile.cl 
+
+13. Oficina Administrativa:
+La Oficina Administrativa, parte de la Dirección Económica y Administrativa de la Facultad, es la unidad encargada de gestionar el control de bienes e inventario, gestionar las importaciones con agentes de aduanas solicitados por el estamento académico, así como gestionar los seguros involucrados de los diferentes equipos adquiridos, gestionar los pagos a proveedores solicitados por las diferentes unidades a cargo del Decanato y Vicedecanato.
+Integrantes:
+Jefe Administrativo: Luis Ernesto Soto Lorca
+Encargada de Control de Bienes e Inventario: Katherine Ortiz Plaza
+Encargado Importaciones y Seguros: Felipe Marilao González
+Encargado de proveedores: Hugo Mora González
+Contacto:	
++562 29784487, +562 29777485, +562 29784445, +562 29777479
+Beauchef 850, Santiago. Edificio Justicia Espada, 6° piso.
+
+14. Unidad Jurídica:
+La Unidad Jurídica, dependiente del Decanato, tiene como función principal asesorar al decano y equipo directivo en aspectos jurídicos; asesorar a departamentos y unidades en materias de compras, convenios de colaboración y contratos, aplicación normativa sector público e institucional,requerimientos CGR, CI, SES, Sernac y otras instituciones gubernamentales. En materia de sumarios administrativos e investigaciones sumarias asesora a fiscales y actuarios, y es la responsable de la sistematización y custodia de expedientes, elaboración de actos vinculados, entre otras funciones. También le corresponde asesorar, coordinar y elaborar entrega de información en virtud de solicitudes por Ley de Transparencia. Integra la Comisión Asesora de Resoluciones del Decano (CARD) y otras comisiones en representación de las autoridades.
+Integrantes:
+Jefa unidad: Paulina Fuentes Viveros
+Abogada: Muriel Vilches Carreño
+Abogada: Claudia Molina Vasquez
+Abogada: Graciela Garay Ulloa
+Abogado: Claudio Toledo Castillo
+Abogado: Gabriel Guerrero Pérez
+Secretaria: Paola Silva Tomea
+Contacto:
++56 2 2978 4432
+Beauchef 850, Santiago. Edificio Justicia Espada, 7mo piso
+juridicafcfm@uchile.cl
 `;
